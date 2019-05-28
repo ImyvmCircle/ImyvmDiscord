57d24585f45577599673112afb1eb3a951a7f8d1
huangwang1003 <huangwang1003@gmail.com>
2019-05-28 17:37:07 +0800

    Upload project

---
+++ src/com/imyvm/spigot/ImyvmDiscord/ChatListener.java
@@ -0,0 +1,83 @@
+package com.imyvm.spigot.ImyvmDiscord;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class ChatListener implements Listener {
+
+    final private ImyvmDiscord plugin;
+
+    public ChatListener(ImyvmDiscord pl) {
+        plugin = pl;
+        plugin.getServer().getPluginManager().registerEvents(this, pl);
+    }
+
+    @EventHandler
+    public void onPlayerCommand(PlayerCommandPreprocessEvent event){
+        FileConfiguration config = plugin.getConfig();
+        String prefix = config.getString("Prefix");
+        String token = config.getString("Token-ID");
+        boolean opOverride = config.getBoolean("OpOverride");
+        List<String> boldCommands = config.getStringList("BoldCommands");
+        List<String> ignoreCommands = config.getStringList("IgnoreCommands");
+
+        String commands = event.getMessage();
+        if (event.isCancelled()){
+            return;
+        }
+        if (event.getPlayer().isOp() && opOverride){
+            return;
+        }
+        if (listContain(ignoreCommands, commands)){
+            return;
+        }
+        if (listContain(boldCommands, commands)){
+            commands = "**"+commands+"**";
+        }
+        String message = prefix+" "+event.getPlayer().getName()+": "+commands;
+        CompletableFuture.runAsync(() -> { sendMessage(token, message); });
+    }
+
+    private static void sendMessage(String webhook_url, String content) {
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpResponse response;
+        HttpPost request = new HttpPost(webhook_url);
+        request.addHeader("Content-Type", "application/json");
+        String jsonMessage = "{\"content\": \"" + content + "\"}";
+        try {
+            StringEntity params = new StringEntity(jsonMessage, java.nio.charset.Charset.forName("UTF-8"));
+            request.setEntity(params);
+            response = httpClient.execute(request);
+        }catch (IOException ex) {
+            ex.printStackTrace();
+            return;
+        }
+        if (!(response.getStatusLine().getStatusCode()==204)){
+            System.out.println(response.getStatusLine());
+        }
+    }
+
+    private boolean listContain(List<String> commandsList, String commands){
+        if (commandsList.isEmpty()){
+            return false;
+        }
+        if (!commands.contains(" ")){
+            return commandsList.contains(commands);
+        }else {
+            return commandsList.contains(commands.substring(0, commands.indexOf(" ")));
+        }
+    }
+}
---
+++ src/com/imyvm/spigot/ImyvmDiscord/ImyvmDiscord.java
@@ -0,0 +1,72 @@
+package com.imyvm.spigot.ImyvmDiscord;
+
+import com.google.common.base.Charsets;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class ImyvmDiscord extends JavaPlugin implements CommandExecutor{
+
+    private static final Logger log = Logger.getLogger("Acquisition");
+    private FileConfiguration config = getConfig();
+
+    public ChatListener chatListener;
+
+    private List<String> boldCommands = new ArrayList<>();
+    private List<String> ignoreCommands = new ArrayList<>();
+
+
+    @Override
+    public void onDisable() {
+        log.info(String.format("[%s] Disabled Version %s", getDescription().getName(),
+                getDescription().getVersion()));
+    }
+
+    @Override
+    public void onEnable() {
+        log.info(String.format("[%s] Enabled Version %s", getDescription().getName(), getDescription().getVersion()));
+
+        config.addDefault("Prefix", "[World]");
+        config.addDefault("Token-ID",
+                "https://discordapp.com/api/webhooks/xxxxxxxxxxxxx/" +
+                        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        config.addDefault("OpOverride", true);
+        boldCommands.add("/tp");
+        ignoreCommands.add("/msg");
+        config.addDefault("BoldCommands",boldCommands);
+        config.addDefault("IgnoreCommands", ignoreCommands);
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        chatListener = new ChatListener(this);
+
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if ((sender instanceof ConsoleCommandSender) || sender.hasPermission("imyvmdiscord.reload")) {
+            if (args.length==0){
+                sender.sendMessage(ChatColor.GREEN + "---- ImyvmDiscord ----");
+                sender.sendMessage(ChatColor.GREEN + "- /imyvmdiscord reload    - reload this plugin");
+            }
+            else if (args[0].equalsIgnoreCase("reload") && args.length==1) {
+                reloadConfig();
+                sender.sendMessage(ChatColor.GREEN + "Configuration Reloaded!");
+            } else {
+                sender.sendMessage(ChatColor.GREEN + "---- ImyvmDiscord ----");
+                sender.sendMessage(ChatColor.GREEN + "- /imyvmdiscord reload    - reload this plugin");
+            }
+        } else {
+            sender.sendMessage(ChatColor.RED + "You don't have the permission!");
+        }
+        return true;
+    }
+}
